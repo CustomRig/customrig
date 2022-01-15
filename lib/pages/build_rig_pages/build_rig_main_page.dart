da49d9c3f418e4170178ab94f92b1d0481097826
Varad Gauthankar <gvarad1601@gmail.com>
2022-01-15 23:30:19 +0530

    made empty files for each page

--- lib/pages/build_rig_pages/build_rig_main_page.dart
+++ lib/pages/build_rig_pages/build_rig_main_page.dart
@@ -1,3 +1,4 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 class BuildRigMainPage extends StatefulWidget {
@@ -13,6 +14,17 @@ class _BuildRigMainPageState extends State<BuildRigMainPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Custom Build'),
+        bottom: const PreferredSize(
+          child: LinearProgressIndicator(value: 2 / 10),
+          preferredSize: Size.fromHeight(6.0),
+        ),
+      ),
+
+      //
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(EvaIcons.chevronRight),
       ),
     );
   }
