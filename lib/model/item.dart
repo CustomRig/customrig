c862753f1a0930afb1f1feb14349e092ad2daa59
Varad Gauthankar <gvarad1601@gmail.com>
2022-01-23 23:21:57 +0530

    create item model

---
+++ lib/model/item.dart
@@ -0,0 +1,49 @@
+class Item {
+  String? brand;
+  String? title;
+  String? description;
+  int? price;
+  String? imageUrl;
+  String? purchaseUrl;
+  List<int>? pairingIds;
+  String? type;
+  String? id;
+
+  Item({
+    this.brand,
+    this.title,
+    this.description,
+    this.price,
+    this.imageUrl,
+    this.purchaseUrl,
+    this.pairingIds,
+    this.type,
+    this.id,
+  });
+
+  Item.fromJson(Map<String, dynamic> json) {
+    brand = json['brand'];
+    title = json['title'];
+    description = json['description'];
+    price = json['price'];
+    imageUrl = json['image_url'];
+    purchaseUrl = json['purchase_url'];
+    pairingIds = json['pairing_ids'].cast<int>();
+    type = json['type'];
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['brand'] = brand;
+    data['title'] = title;
+    data['description'] = description;
+    data['price'] = price;
+    data['image_url'] = imageUrl;
+    data['purchase_url'] = purchaseUrl;
+    data['pairing_ids'] = pairingIds;
+    data['type'] = type;
+    data['id'] = id;
+    return data;
+  }
+}
