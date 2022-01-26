16218b1bc7f8b8807787b734fbae35713157be03
Varad Gauthankar <gvarad1601@gmail.com>
2022-01-27 00:28:16 +0530

    chnaged ip address

--- lib/global/dio/dio.dart
+++ lib/global/dio/dio.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 // ignore: avoid_classes_with_only_static_members
 class MyDio {
-  static const baseUrl = 'http://192.168.0.101:3000/api'; //local ip
+  static const baseUrl = 'http://192.168.0.130:3000/api'; //local ip
   static BaseOptions options = BaseOptions(baseUrl: baseUrl);
 
   static Future<Dio> provideDio() async {
