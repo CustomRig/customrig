2f5f0b8bb5da6cf464122e073fcc5c10f8253553
Varad Gauthankar <gvarad1601@gmail.com>
2022-06-28 21:21:33 +0530

    checking for internet connection

--- lib/widgets/global_widgets/exception_widget.dart
+++ lib/widgets/global_widgets/exception_widget.dart
@@ -1,27 +1,64 @@
+import 'dart:io';
+
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 import '../../utils/text_styles.dart';
 
-class ExceptionWidget extends StatelessWidget {
+class ExceptionWidget extends StatefulWidget {
   const ExceptionWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ExceptionWidget> createState() => _ExceptionWidgetState();
+}
+
+class _ExceptionWidgetState extends State<ExceptionWidget> {
+  bool isInternet = true;
+
+  Future<void> _isInternet() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+
+      if (result.isNotEmpty) {
+        setState(() {
+          isInternet = true;
+        });
+      } else {
+        setState(() {
+          isInternet = false;
+        });
+      }
+    } on SocketException catch (_) {
+      setState(() {
+        isInternet = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _isInternet();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Column(
       mainAxisSize: MainAxisSize.min,
-      children: const [
-        Icon(
+      children: [
+        const Icon(
           EvaIcons.alertTriangleOutline,
           size: 28,
         ),
         Text(
-          'Something went wrong!',
+          isInternet ? 'Something went wrong!' : 'No internet connection!',
           style: MyTextStyles.productTitle,
         ),
         Text(
-          'Please try again later.',
+          isInternet
+              ? 'Please try again later.'
+              : 'Please connect to internet.',
           style: MyTextStyles.productSubtitle,
         ),
       ],
